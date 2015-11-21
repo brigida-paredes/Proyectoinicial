c807a0bfe5cb1373df5a744b6cafc387d736eb04
brigida-paredes <brigida.paredes@hotmail.com>
2015-11-21 19:13:29 +0100

    ejemplos de funciones

---
+++ routes/ejemplo17.js
@@ -0,0 +1,14 @@
+function multiplicar (num)
+{
+var n=10;
+for (var i=1;i<=n;i++)
+{
+		
+		console.log(num+"*"+i+"="+num*i);
+
+}	
+
+}
+var multiplicador=4
+console.log("tabla del  " + multiplicador);
+multiplicar(multiplicador);
---
+++ routes/ejemplo18.js
@@ -0,0 +1,12 @@
+pelicula 
+	genero
+	nombre
+	precio
+	tiempo
+var pelicula={ genero:'drama',
+nombre:'pelicula1',
+precio:'30',
+tiempo:'60',
+};
+console.log ("pelicula:"+pelicula.nombre);
+
